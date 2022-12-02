d8db346774c9f2a2d418969b1779cdfa9ef7f155
flixha <felix.halpaap@uib.no>
2022-12-02 16:46:43 +0100

    use quick copy function on streams

--- eqcorrscan/core/match_filter/matched_filter.py
+++ eqcorrscan/core/match_filter/matched_filter.py
@@ -27,7 +27,7 @@ from eqcorrscan.core.match_filter.helpers import (
 from eqcorrscan.utils.correlate import get_stream_xcorr
 from eqcorrscan.utils.findpeaks import multi_find_peaks
 from eqcorrscan.utils.pre_processing import (
-    dayproc, shortproc, _prep_data_for_correlation)
+    dayproc, shortproc, _prep_data_for_correlation, _quick_copy_stream)
 from eqcorrscan.core.template_gen import _rms
 from eqcorrscan.core.match_filter.detection import Detection
 from eqcorrscan.utils.plotting import _match_filter_plot
@@ -788,8 +788,10 @@ def match_filter(template_names, template_list, st, threshold,
     if copy_data:
         # Copy the stream here because we will muck about with it
         Logger.info("Copying data to keep your input safe")
-        stream = st.copy()
-        templates = [t.copy() for t in template_list]
+        # stream = st.copy()
+        stream = _quick_copy_stream(st)
+        # templates = [t.copy() for t in template_list]
+        templates = [_quick_copy_stream(t) for t in template_list]
         _template_names = template_names.copy()  # This can be a shallow copy
     else:
         stream, templates, _template_names = st, template_list, template_names
