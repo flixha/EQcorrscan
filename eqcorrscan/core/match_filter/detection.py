2c28a0fe8b18acdcd0d6b74b2afc8302fd1fec49
flixha <felix.halpaap@uib.no>
2023-01-05 16:22:14 +0100

    add comments and adjust tests where output changed a bit due to the window in which SNR is calculated here

--- eqcorrscan/core/match_filter/detection.py
+++ eqcorrscan/core/match_filter/detection.py
@@ -15,7 +15,6 @@ import ast
 import copy
 import os
 import logging
-import warnings
 
 import numpy as np
 from obspy import Catalog, UTCDateTime, Stream
@@ -23,13 +22,11 @@ from obspy.core.event import (
     Comment, WaveformStreamID, Event, Pick, CreationInfo, ResourceIdentifier,
     Origin)
 from obspy.core.util import AttribDict
-from obspy.core.util.deprecation_helpers import ObsPyDeprecationWarning
 
 from eqcorrscan.core.match_filter.helpers import _test_event_similarity
 from eqcorrscan.utils.pre_processing import _stream_quick_select
 
 Logger = logging.getLogger(__name__)
-warnings.filterwarnings("ignore", category=ObsPyDeprecationWarning)
 
 
 class SparsePick(object):
